aace95d06774e8e9a826599361342367f0d49790
Vladimir Fomene <vladimirfomene@gmail.com>
2018-10-13 18:29:46 +0100

    Bug fixes

--- stat.py
+++ stat.py
@@ -3,10 +3,14 @@ import requests
 import constant
 import sys
 
+GITHUB_API_URL = "https://api.github.com"
+
 def get_repo_path(args):
     clone_url = args[1]
     parsed_url = urlparse(clone_url)
-    path = str(parsed_url.path).[:-4]
+    path = str(parsed_url.path)
+    path = path[:-4]
+    
     return path
 
 
@@ -18,20 +22,19 @@ def make_api_call(url):
     resp = None
 
     try:
-        resp.json()
+        resp = req.json()
     except Exception as e:
         print("ValueError: No JSON object could be decoded")
-
     return resp
 
 def print_response(resp):
-    print("Repository: " + resp.name)
-    print("Description: " + resp.description)
-    print("Language: " + resp.language)
-    print("Size: " + int(resp.size) / 1000 + " MB")
-    print("Stars: " + resp.stargazers_count)
-    print("Watchers: " + resp.watchers_count)
-    print("Forks: " + resp.forks)
+    print("Repository: " + resp['name'])
+    print("Description: " + resp['description'])
+    print("Language: " + resp['language'])
+    print("Size: " + str(int(resp['size']) / 1000) + " MB")
+    print("Stars: " + str(resp['stargazers_count']))
+    print("Watchers: " + str(resp['watchers_count']))
+    print("Forks: " + str(resp['forks']))
 
 
 def main():
@@ -39,5 +42,5 @@ def main():
 
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
